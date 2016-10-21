bafb45a9b6530d459d432d2305ac1acebab2c5a0
stevenaldous <stevenaldous@gmail.com>
2016-10-21 14:34:27 +0800

    second commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+// slides for carousal
+  // var slides = [
+  //   {image:'./images/shred_wa.png', title:'Shred WA',text:'Shred WA was designed to be the one stop weather application to help you shred the great state of Washington by helping find Ski Resorts and Mountain Bike Trails and includes the current weather as well as a forecast. Shred WA was built with AngularJS, Sails, MongoDb, Bootstrap, Trails.API, and the OpenWeather API.',url:'https://shredwa.herokuapp.com/'},
+  //   {image:'./images/wecycle.png', title:'WEcycle',text:'WEcycle is a community driven application to connect individuals who are interested in recycling with each other. Users can post their reuse ideas as well as search their address to find out what dates their recycling needs to be taken to the curb. WEcycle was built with Ruby on Rails and utilizes Rest Client to pull calendar information from Seattle.gov.',url:'https://rocky-falls-6405.herokuapp.com/'},
+  //   {image:'./images/priceIsRight.png', title:'Price-is-right',text:'The Price-Is Right is a two-player consumertrivia game based off of the long running show of the same name. Players take turns toguess the price of an item and the first to 10answers wins!Price-Is Right is an application built on pure Javascript.',url:'http://stevenaldous.github.io/price-is-right/'}
+  //   // {image:'./images/heywanna.png', title:'HEYWANNA',text:'HEYWANNA is designed to help a user plan a memorable date to make a truly outstanding impression. Users can create, store, and send dates from their account. HEYWANNA was built on NodeJS and Express and utilizes the MapBox, Foursquare, and Eventful APIs.',url:'http://heywanna.herokuapp.com/'}
+  // ];
+  $('.carousel').carousel({
+    interval: 10000
+  });
+
+
+});
